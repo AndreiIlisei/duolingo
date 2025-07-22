70e9e75cdc1926817b8c9ca99fb7cf73d72c4e67
Andrei Ilisei <andrei.sv97@yahoo.com>
2025-07-22 15:12:38 +0200

    add env to next for build purposes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 const nextConfig = {
   output: "standalone",
 
+  // Make these available at build time and runtime
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+      process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    STRIPE_API_KEY: process.env.STRIPE_API_KEY,
+    STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET,
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+  },
+
   async headers() {
     return [
       {
